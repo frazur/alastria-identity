c43bdf20a440028881c2fced99838c3f3c70c0ce
Francesco Zurolo <frazur@gmail.com>
2020-11-18 09:50:33 +0100

    **FIX** truffle.js

--- truffle.js
+++ truffle.js
@@ -12,11 +12,11 @@ module.exports = {
       gas: 0xffffff,
       gasPrice: 0x0,
       from: "0xd65616c46a2e55957aff33e238b31bc568358e20"
-    },
-    compilers: {
-      solc: {
-        version: "^0.4.23", // A version or constraint - Ex. "^0.5.0"
-      }
+    }
+  },
+  compilers: {
+    solc: {
+      version: "^0.4.23", // A version or constraint - Ex. "^0.5.0"
     }
   }
 
